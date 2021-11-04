edada68499877590982f0f982ad83156182d09f9
Martijn Smit <martijn@lostdomain.org>
2021-11-04 18:59:11 +0100

    Add correct language prefixes for French & Slovak

--- whatpulse_sk.ts
+++ whatpulse_sk.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ts_ZA">
+<TS version="2.1" language="sk_SK">
 <context>
     <name>AccountTab</name>
     <message>
